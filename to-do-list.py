dc16ca1c0cb1beb8627d8e00b74388574773fa2b
sean15535 <sean15535@gmail.com>
2024-02-29 09:47:09 +0100

    preparing the code

---
+++ to-do-list.py
@@ -0,0 +1,7 @@
+todo_list = []
+print("Welcome to To-do List App")
+while true:
+    print("What are your plans for today?")
+    print("\n1. Display To-Do List. \n2. Add Task. \n3. Mark Task as Completed. \n4. Remove Task. \n5. Exit")
+    choice = int((input(">> ")))
+            
